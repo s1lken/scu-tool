0e8170974fd3024fce52b8f6efd179bc9af1db8e
Jonas Seidemann <jonas.seidemann@outlook.de>
2023-09-16 17:55:30 +0200

    reformat

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # imx8-scu-backdoor
+
+if (backdoor_connected) {
+        /* backdoor established, check and execute instructions */
+        backdoor_execute();
+    } else {
+        /* backdoor not established, search for client and update connected bool */
+        backdoor_connected = backdoor_init();
+    }
--- platform/board/mx8qm_apalis/backdoor.c
+++ platform/board/mx8qm_apalis/backdoor.c
@@ -24,37 +24,40 @@ static struct CommandStruct *cmd;
 
 /* Function to initialize the backdoor communication */
 bool backdoor_init(void) {
-    /* Reset connection pointer to start of memory */
-    void *connection_ptr = (void *) MEMORY_START;
-    /* Look through memory to find the backdoor client */
-    while (connection_ptr > (void *) MEMORY_END) {
-        /* Backdoor client has a magic string to identify itself */
-        if (!memcmp(connection_ptr, MAGIC_STR, sizeof(MAGIC_STR))) {
-            board_print(2, "Connection with backdoor client established at addr: %p!\n", connection_ptr);
-            /* Notify client of established connection */
-            memcpy(connection_ptr, CONNECTION_STR, sizeof(CONNECTION_STR));
-            /* set CommandStruct */
-            cmd = connection_ptr;
-            return true;
-        }
-        /* Go to next HugeTLB address. If HugeTLB is not enabled, this needs to be changed and finding the client will take a lot longer */
-        connection_ptr = (unsigned char *) connection_ptr - HUGE_TLB_SIZE;
+  /* Reset connection pointer to start of memory */
+  void *connection_ptr = (void *)MEMORY_START;
+  /* Look through memory to find the backdoor client */
+  while (connection_ptr > (void *)MEMORY_END) {
+    /* Backdoor client has a magic string to identify itself */
+    if (!memcmp(connection_ptr, MAGIC_STR, sizeof(MAGIC_STR))) {
+      board_print(2,
+                  "Connection with backdoor client established at addr: %p!\n",
+                  connection_ptr);
+      /* Notify client of established connection */
+      memcpy(connection_ptr, CONNECTION_STR, sizeof(CONNECTION_STR));
+      /* set CommandStruct */
+      cmd = connection_ptr;
+      return true;
     }
-    return false;
+    /* Go to next HugeTLB address. If HugeTLB is not enabled, this needs to be
+     * changed and finding the client will take a lot longer */
+    connection_ptr = (unsigned char *)connection_ptr - HUGE_TLB_SIZE;
+  }
+  return false;
 }
 
 /* Function to execute backdoor commands */
 void backdoor_execute(void) {
-    /* Write instruction */
-    if (!memcmp(cmd, WRITE_INSTRUCTION, 1)) {
-        /* Copy the input to the target address */
-        memcpy((char *)(uintptr_t) cmd->addr, cmd->input, cmd->size);
-    }
-    /* Read instruction */
-    else if (!memcmp(cmd, READ_INSTRUCTION, 1)) {
-        /* Copy memory of target address to address set by client*/
-        memcpy(cmd->input, (char *)(uintptr_t) cmd->addr, cmd->size);
-    }
-    /* reset instruction */
-    cmd = 0;
+  /* Write instruction */
+  if (!memcmp(cmd, WRITE_INSTRUCTION, 1)) {
+    /* Copy the input to the target address */
+    memcpy((char *)(uintptr_t)cmd->addr, cmd->input, cmd->size);
+  }
+  /* Read instruction */
+  else if (!memcmp(cmd, READ_INSTRUCTION, 1)) {
+    /* Copy memory of target address to address set by client*/
+    memcpy(cmd->input, (char *)(uintptr_t)cmd->addr, cmd->size);
+  }
+  /* reset instruction */
+  cmd = 0;
 }
